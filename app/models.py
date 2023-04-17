180ffb0a01f6bc41cfb43d604403e4c8c0b537ed
Home <igor.kiyashko@inbox.ru>
2023-04-17 22:34:07 +0600

    Initial commit

---
+++ app/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Transaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    created=models.DateField(auto_now_add=True)
+    modified=models.DateField(auto_now_add=True)
+    description=models.CharField(max_length=100, blank=True, null=True)
+    amount=models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.description}: {self.amount} KZT"
+
+
+# Create your models here.
